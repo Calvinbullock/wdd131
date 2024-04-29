b0dfca17b5accda9d18395a419d5fda08223eaec
Calvinbullock <calvinnbullock@gmail.com>
2024-04-29 11:52:01 -0600

    scripts file
    forgot to git add it last commit

---
+++ mission/scripts.js
@@ -0,0 +1,14 @@
+console.log("testing")
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.getElementById('theme')
+
+    selectElement.addEventListener('change', function () {
+        const selectedValue =this.value
+        if (selectedValue == 'dark') {
+            document.body.classList.add('dark')
+        } else {
+            document.body.classList.remove('dark')
+        }
+    })
+})
