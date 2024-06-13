70b978f36ef10f32f58a7964913f797cd3c8c67a
Calvinbullock <calvinnbullock@gmail.com>
2024-06-13 09:05:02 -0600

    register should work now

--- register/register.js
+++ register/register.js
@@ -1,3 +1,6 @@
+
+import { participantTemplate, successesTemplate } from "./templates.js";
+
 /* ==============================================
  * ----------------------------------------------
  *              Create Participant Form
@@ -9,49 +12,6 @@ let numberOfParticipants = 1;
 
 const addParticipantButton = document.getElementById("add");
 const submitButton = document.getElementById("submitButton");
-
-/* ==============================================
- *
- * ============================================ */
-function participantTemplate(count) {
-    return `<section class="participant${count}">
-        <p>Participant ${count}</p>
-        <div class="item">
-            <label for="fname"> First Name<span>*</span></label>
-            <input id="fname${count}" type="text" name="fname" value="" required />
-        </div>
-        <div class="item activities">
-            <label for="activity${count}">Activity #<span>*</span></label>
-            <input id="activity${count}" type="text" name="activity" />
-        </div>
-        <div class="item">
-            <label for="fee${count}">Fee ($)<span>*</span></label>
-            <input id="fee${count}" type="number" name="fee" />
-        </div>
-        <div class="item">
-            <label for="date${count}">Desired Date <span>*</span></label>
-            <input id="date${count}" type="date" name="date" />
-        </div>
-        <div class="item">
-            <p>Grade</p>
-            <select>
-                <option selected value="" disabled selected></option>
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
-                <option value="5">5th</option>
-                <option value="6">6th</option>
-                <option value="7">7th</option>
-                <option value="8">8th</option>
-                <option value="9">9th</option>
-                <option value="10">10th</option>
-                <option value="11">11th</option>
-                <option value="12">12th</option>
-            </select>
-        </div>
-    </section>`
-}
 
 /* ==============================================
  *
@@ -106,20 +66,6 @@ function getData() {
         participent_count: numberOfParticipants,
         total_fees: totalFees(),
     }
-}
-
-/* ==============================================
- *
- * ============================================ */
-function successesTemplate(data) {
-    return `
-        <div id="successesDiv">
-            <h1>Submisstion Details</h1>
-            <p>Thank you ${data.adult_name} for registering.</p>
-            <p>You have registered ${data.participent_count} participants and</p>
-            <p>owe $ ${data.total_fees} in Fees.</p>
-        </div>
-        `
 }
 
 /* ==============================================
