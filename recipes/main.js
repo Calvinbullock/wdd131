25ff1c1c982d2bc4bf97ab52ef3e28d05343417c
Calvinbullock <calvinnbullock@gmail.com>
2024-06-24 10:52:37 -0600

    recipe main.js savepoint

---
+++ recipes/main.js
@@ -0,0 +1,109 @@
+import recipes from './recipes.mjs';
+
+/* ==========================================================
+ * Create and return a tags template
+ ========================================================= */
+function tagsTemplate(tags) {
+    // loop through the tags list and transform the strings to HTML
+    let tagsHTML = ``
+    for (let i = 0; i < tags.length; i++) {
+        tagsHTML += `<li>${tags[i]}</li>`
+    }
+
+	return tagsHTML;
+}
+
+/* ==========================================================
+ * Create and return a rating template
+ ========================================================= */
+function ratingTemplate(rating) {
+	// begin building an html string using the ratings HTML written earlier as a model.
+	let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5 stars"
+    >`
+
+    for (let i = 1; i < 6; i++) {
+        html += `\n`
+        if (i >= rating) {
+            html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`
+        } else {
+            html += `<span aria-hidden="true" class="icon-star">⭐</span>`
+        }
+    }
+	html += `\n</span>`
+
+	return html
+}
+
+/* ==========================================================
+ * Create a template
+ ========================================================= */
+function recipeTemplate(recipe) {
+    return  `
+        <div class="recipe-card">
+            <div class="recipe-img-div">
+                <img class="recipe-img" src="${recipe.image}" alt="${recipes.description}">
+            </div>
+                <div class="recipe-details">
+                <div class="recipe-tags">
+                    ${tagsTemplate(recipe.tags)}
+                </div>
+                    <h2 class="recipe-name">${recipe.name}</h2>
+                    <span
+                        class="rating"
+                        role="img"
+                        aria-label="Rating: 4 out of 5 stars"
+                    >
+                    ${ratingTemplate(recipe.rating)}
+                    <p class="desc">
+                        ${recipe.description}
+                    </p>
+                </span>
+            </div>
+        </div>`
+}
+
+/* ==========================================================
+ * Get and return and random recipe number
+ ========================================================= */
+function getRandRecipe() {
+    let randInt = Math.floor(Math.random()* recipes.length)
+    return recipeTemplate(recipes[randInt])
+}
+
+/* ==========================================================
+ * 
+ ========================================================= */
+function renderRecipes(recipeList) {
+	// get the element we will output the recipes into
+    const recipeDiv = document.getElementById("recipe-div");
+
+	// use the recipeTemplate function to transform our recipe objects into recipe HTML strings
+    let recipesHTML = ``
+    for (let i = 0; i < recipeList.length; i++) {
+        recipesHTML += recipeTemplate(i)
+    }
+    
+	// Set the HTML strings as the innerHTML of our output element.
+    const successesView = document.createElement("div");
+    successesView.innerHTML = successesTemplateEx(recipesHTML)
+
+    recipeDiv.parentNode.insertBefore(recipesHTML, recipeDiv);
+}
+
+/* ==========================================================
+ * 
+ ========================================================= */
+function init() {
+  // get a random recipe
+  const recipe = getRandRecipe(recipes)
+  // render the recipe with renderRecipes.
+  renderRecipes([recipe]);
+}
+init();
+
+//let recipeHTML = randRecipe()
+//console.log(recipeHTML)
+
