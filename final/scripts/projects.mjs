f71eb275c61b8b8c0a0288d38126a45a9cbde924
Calvinbullock <calvinnbullock@gmail.com>
2024-07-12 21:14:34 -0600

    removed unneded data

--- final/scripts/projects.mjs
+++ final/scripts/projects.mjs
@@ -5,14 +5,11 @@ const projects = [
         gitUrl: 'https://github.com/Calvinbullock/Chess',
         startDate: 'May 18, 2022',
         lastUpdate: '--',
-        datePublished: null,
-
         desc: 'This is one of my first programs, I made it back in  May 18, 2022',
         longDesc: '',
         heroImage: './images/Chess-Game-Play.png',
         altText: "Game running",
         youtubeUrl: null,
-        otherImages: [null],
         tags: ['c++', 'games', 'oop', 'terminal', 'PersinalProject'],
     },
     {
@@ -21,18 +18,22 @@ const projects = [
         gitUrl: 'https://github.com/Calvinbullock/new-life',
         startDate: 'Sep 2, 2023',
         lastUpdate: '--',
-
-        datePublished: null,
         desc: 'This is a adventure tile game. It is built using c++ and the SFML library.',
-
         longDesc: '\tI got the idea from the object orietaed class I took in my 4th semester. My final was building a simple turnbased game. I took this idea and originally planned to exspand on it. But as I started I decided to make it a whole new thing. This game is what came from that.<br><br>\tI am still working on this project when I can find time between classwork.',
-
         heroImage: './images/New-Life-Game-Play.png',
         altText: "Game running",
         youtubeUrl: 'https://www.youtube.com/watch?v=TKum7AfY2Do',
-        otherImages: [null],
         tags: ['c++', 'games', 'oop', 'gui', 'PersinalProject'],
     },
 ]
 
 export default projects 
+
+
+// TODO  
+/*
+ * Move header / footer to template funcs
+ * build search functions
+ * add more project data
+ * swap <leader>p and <leader>P in vim
+ * */
