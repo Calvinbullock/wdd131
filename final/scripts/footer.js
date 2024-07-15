9aa22e3d565adb007b641904e137ae3cd60720eb
Calvinbullock <calvinnbullock@gmail.com>
2024-07-15 12:50:57 -0600

    added a js file to build footer

---
+++ final/scripts/footer.js
@@ -0,0 +1,35 @@
+
+/* ==========================================================
+ * Footer Template
+ ========================================================= */
+function footerTemplate() {
+    return `
+            <hr id="footer-hr">
+            <div id="footer-flex">
+                <div class="socal-links">
+                    <a href="https://www.linkedin.com/in/calvin-bullock/"><img class="social-logo" src="images/LinkIn-logo.jpg" alt="in logo"></a>
+                    <a href="https://github.com/Calvinbullock"><img class="social-logo" src="images/git-hub-logo.jpg" alt="github logo"></a>
+                </div>
+                <div>
+                    <p>@Calvin</p>
+                </div>
+            </div>`
+}
+
+/* ==========================================================
+ * Render function
+ ========================================================= */
+function renderFooter() {
+    console.log("pop")
+    const outputElement = document.querySelector("footer")
+
+    outputElement.innerHTML = footerTemplate() 
+}
+
+/* ==========================================================
+ * init function
+ ========================================================= */
+function init() {
+    renderFooter()
+}
+init()
