47f9b28623b4ff008e8ed4eaec728d93fec102d0
Calvinbullock <calvinnbullock@gmail.com>
2024-07-15 12:59:50 -0600

    footer social links now open in new tab

--- final/scripts/footer.js
+++ final/scripts/footer.js
@@ -7,8 +7,8 @@ function footerTemplate() {
             <hr id="footer-hr">
             <div id="footer-flex">
                 <div class="socal-links">
-                    <a href="https://www.linkedin.com/in/calvin-bullock/"><img class="social-logo" src="images/LinkIn-logo.jpg" alt="in logo"></a>
-                    <a href="https://github.com/Calvinbullock"><img class="social-logo" src="images/git-hub-logo.jpg" alt="github logo"></a>
+                    <a href="https://www.linkedin.com/in/calvin-bullock/" target="_blank"><img class="social-logo" src="images/LinkIn-logo.jpg" alt="in logo"></a>
+                    <a href="https://github.com/Calvinbullock" target="_blank"><img class="social-logo" src="images/git-hub-logo.jpg" alt="github logo"></a>
                 </div>
                 <div>
                     <p>@Calvin</p>
